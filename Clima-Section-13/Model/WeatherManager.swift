183d81184af9995eede336209c145b020bb5b497
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-08 22:53:30 +0300

    Modifies .gitignore and hides api key

--- Clima-Section-13/Model/WeatherManager.swift
+++ Clima-Section-13/Model/WeatherManager.swift
@@ -5,8 +5,10 @@
 //  Created by Abdurahman on 8.11.2023.
 //
 
+import Foundation
+
 struct WeatherManager {
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=6494a3072a3cc7a17e0a98969f869ac9&units=metric"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=\(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "")&units=metric"
     
     func fetchWeather (cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"

16f450556fd724abdc9289971528116462755bc5
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-09 10:39:08 +0300

    Uses URLSession for networking

--- Clima-Section-13/Model/WeatherManager.swift
+++ Clima-Section-13/Model/WeatherManager.swift
@@ -7,11 +7,35 @@
 
 import Foundation
 
+let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? ""
+
 struct WeatherManager {
-    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=\(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "")&units=metric"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=\(apiKey)&units=metric"
     
     func fetchWeather (cityName: String) {
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        //  1. Create a Url:
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession:
+            let session = URLSession(configuration: .default)
+            // 3. Give the session a task:
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            // 4. Start the task:
+            task.resume()
+        }
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?)->Void{
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
     }
 }
